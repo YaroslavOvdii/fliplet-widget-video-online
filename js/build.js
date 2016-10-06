aa57100fb5d659ec5a769d1a95c7505b079e7b09
Ricardo Seromenho <ricardo@seromenho.com>
2016-10-06 14:31:03 +0100

    Add build logic

---
+++ js/build.js
@@ -0,0 +1,16 @@
+$('[data-video-online-id]').each(function () {
+  var $el = $(this);
+
+  var data = Fliplet.Widget.getData($el.data('video-online-id'));
+  $el.find('img')
+    .on('click', function() {
+      if (Fliplet.Navigator.isOnline()) {
+        $el.html(data.video_html);
+      } else {
+        Fliplet.Navigator.popup({
+          popupTitle: 'Internet Unavailable',
+          popupMessage: 'This video requires Internet to play. Please try again when Internet is available.'
+        });
+      }
+    });
+});

602405082049a98027f2dee105488b00992253c7
Ricardo Seromenho <ricardo@seromenho.com>
2016-11-02 17:51:23 +0000

    GA events

--- js/build.js
+++ js/build.js
@@ -5,8 +5,10 @@ $('[data-video-online-id]').each(function () {
   $el.find('img')
     .on('click', function() {
       if (Fliplet.Navigator.isOnline()) {
+        Fliplet.Analytics.trackEvent('video', 'play_streaming_online', data.url);
         $el.html(data.video_html);
       } else {
+        Fliplet.Analytics.trackEvent('video', 'play_streaming_offline', data.url);
         Fliplet.Navigate.popup({
           popupTitle: 'Internet Unavailable',
           popupMessage: 'This video requires Internet to play. Please try again when Internet is available.'
